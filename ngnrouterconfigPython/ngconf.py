c398775f910bd6a49a6fb47dc1dc625f59c2550a
Hiroyasu Tanabe <h@cosat.com>
2018-04-30 16:37:18 +0900

    ix_direct 対応

--- ngnrouterconfigPython/ngconf.py
+++ ngnrouterconfigPython/ngconf.py
@@ -7,36 +7,21 @@ __doc__ = """The above defines the script's __doc__ string. You can fix it by li
 
 import sys
 import os
-import ptvsd # 追加ライブラリ
-import pexpect # 追加ライブラリ
+import ptvsd  # 追加ライブラリ
+import pexpect  # 追加ライブラリ
 import time
 import ngnrouterlib
 
 # 環境変数 PYTHON3_PTVSD_SECRET がある場合は リモートデバッグを有効にする
 if ("PYTHON3_PTVSD_SECRET" in os.environ):
-	ptvsd.enable_attach(os.environ['PYTHON3_PTVSD_SECRET'], address = ('0.0.0.0', 3000))
-	ptvsd.wait_for_attach()
-	ptvsd.break_into_debugger()
+    ptvsd.enable_attach(os.environ['PYTHON3_PTVSD_SECRET'], address=('0.0.0.0', 3000))
+    ptvsd.wait_for_attach()
+    ptvsd.break_into_debugger()
 
 if len(sys.argv) < 2:
     print("パラメータファイルを指定して下さい")
     exit(1)
 
-# config の中身を解析する
-routerconfig, sendlines = ngnrouterlib.readrouterconfig(sys.argv[1])
-
-# ログファイル名の作成
-nowstr = time.strftime('%Y%m%d%H%M%S')
-filewithoutext, fileext = os.path.splitext(sys.argv[1])
-logfilename = filewithoutext + "_" + nowstr + ".log"
-
-if routerconfig["routertype"] == "rtx-ssh":
-    ngnrouterlib.rtx_ssh_login(routerconfig, sendlines, logfilename)
-elif routerconfig["routertype"] == "rtx-telnet":
-    pass
-elif routerconfig["routertype"] == "ix-telnet":
-    ngnrouterlib.ix_telnet_login(routerconfig, sendlines, logfilename)
-else:
-    print("unknown routertype: " + routerconfig["routertype"])
+ngnrouterlib.doNgconf(sys.argv[1])
 
 exit(0)
--- ngnrouterconfigPython/ngnrouterlib/__init__.py
+++ ngnrouterconfigPython/ngnrouterlib/__init__.py
@@ -3,8 +3,29 @@ import os
 import sys
 import re
 from time import sleep
-import pexpect # 追加ライブラリ
-import jinja2 # 追加ライブラリ
+import time
+import pexpect  # 追加ライブラリ
+import jinja2  # 追加ライブラリ
+
+
+def doNgconf(filename):
+    # config の中身を解析する
+    routerconfig, sendlines = readrouterconfig(filename)
+    # ログファイル名の作成
+    nowstr = time.strftime('%Y%m%d%H%M%S')
+    filewithoutext, fileext = os.path.splitext(filename)
+    logfilename = filewithoutext + "_" + nowstr + ".log"
+    if routerconfig["routertype"] == "rtx-ssh":
+        rtx_ssh_login(routerconfig, sendlines, logfilename)
+    elif routerconfig["routertype"] == "rtx-telnet":
+        pass
+    elif routerconfig["routertype"] == "ix-telnet":
+        ix_telnet_login(routerconfig, sendlines, logfilename)
+    elif routerconfig["routertype"] == "ix-direct":
+        ix_direct_login(routerconfig, sendlines, logfilename)
+    else:
+        print("unknown routertype: " + routerconfig["routertype"])
+
 
 def expandTemplate(dict, templatedir, outputdir):
     '''
@@ -16,180 +37,252 @@ def expandTemplate(dict, templatedir, outputdir):
     templateLoader = jinja2.FileSystemLoader(searchpath=templatedir)
     templateEnv = jinja2.Environment(loader=templateLoader)
     for templateFilename in templateEnv.list_templates():
-        outputpath = os.path.join(outputdir ,dict["id"] + "_" + templateFilename)
+        outputpath = os.path.join(
+            outputdir, dict["id"] + "_" + templateFilename)
         template = templateEnv.get_template(templateFilename)
         outputText = template.render(dict)
         with open(outputpath, "w") as fp:
-                fp.write(outputText)
-
+            fp.write(outputText)
 
 
 def readrouterconfig(filename):
-	'''
-	readrouterconfig
-	    1行目を JSON として解釈し、辞書にして タプルの第一要素として返す
-		2行目以降を タプルの第二要素として返す
+    '''
+    readrouterconfig
+        1行目を JSON として解釈し、辞書にして タプルの第一要素として返す
+            2行目以降を タプルの第二要素として返す
 
-		1行目のJSONの 値 について、 $ で始まる値については環境変数を参照して展開する
-	'''
-	# 
-	with open(filename, "r") as fp:
-		jsonstr = fp.readline()
-		sendlines = fp.readlines()
-		jsonobj = json.loads(jsonstr)
-		for k,v in jsonobj.items():
-			if (v.startswith("$") and os.getenv(v[1::])):
-				jsonobj[k] = os.getenv(v[1::])
-		return (jsonobj, sendlines)
+            1行目のJSONの 値 について、 $ で始まる値については環境変数を参照して展開する
+    '''
+    #
+    with open(filename, "r") as fp:
+        jsonstr = fp.readline()
+        sendlines = fp.readlines()
+        jsonobj = json.loads(jsonstr)
+        for k, v in jsonobj.items():
+            if (v.startswith("$") and os.getenv(v[1::])):
+                jsonobj[k] = os.getenv(v[1::])
+        return (jsonobj, sendlines)
+
 
 class multifile(object):
-	def __init__(self, files):
-		self._files = files
-	def __getattr__(self, attr, *args):
-		return self._wrap(attr, *args)
-	def _wrap(self, attr, *args):
-		def g(*a, **kw):
-			for f in self._files:
-				res = getattr(f, attr, *args)(*a, **kw)
-			return res
-		return g
+    def __init__(self, files):
+        self._files = files
+
+    def __getattr__(self, attr, *args):
+        return self._wrap(attr, *args)
+
+    def _wrap(self, attr, *args):
+        def g(*a, **kw):
+            for f in self._files:
+                res = getattr(f, attr, *args)(*a, **kw)
+            return res
+        return g
+
 
 def ix_telnet_login(routerconfig, sendlines, logfilename):
-	'''
-	ix_telnet_login
-	    IX にコンフィグ送信し、送信状態をログファイルに記録する関数
-		ファイルの中身は以下のような内容であるこのを期待している。1行目はJSON、2行目以降は ルータに送信したい内容
-		"router": "10.0.0.1", "routertype": "rtx-ssh", "username": "ログインユーザー名", "password": "ログインパスワード", "adminpassword": "管理者パスワード", "centerrouter":"", "centeruser":"", "centerpassword":"" }
-		show config
-	'''
-	sleepspan = 0.5
-	timeout = 5
-	# logfp = open(logfilename, 'wb')
-	logfp = multifile([ sys.stdout.buffer, open(logfilename, 'wb') ])
-	child = pexpect.spawn('telnet ' +  routerconfig["centerrouter"])
-	child.logfile_read = logfp
-	try:
-		# center router login
-		child.expect('login: ', timeout=timeout)
-		sleep(sleepspan)
-		child.send(routerconfig["centeruser"] + "\r")
-		child.expect('Password: ', timeout=timeout)
-		sleep(sleepspan)
-		child.send(routerconfig["centerpassword"] + "\r")
+    '''
+    ix_telnet_login
+        IX にコンフィグ送信し、送信状態をログファイルに記録する関数
+            ファイルの中身は以下のような内容であるこのを期待している。1行目はJSON、2行目以降は ルータに送信したい内容
+            "router": "10.0.0.1", "routertype": "ix_telnet_login", "username": "ログインユーザー名", "password": "ログインパスワード", "adminpassword": "管理者パスワード", "centerrouter":"", "centeruser":"", "centerpassword":"" }
+            show config
+    '''
+    sleepspan = 0.5
+    timeout = 5
+    # logfp = open(logfilename, 'wb')
+    logfp = multifile([sys.stdout.buffer, open(logfilename, 'wb')])
+    child = pexpect.spawn('telnet ' + routerconfig["centerrouter"])
+    child.logfile_read = logfp
+    try:
+        # center router login
+        child.expect('login: ', timeout=timeout)
+        sleep(sleepspan)
+        child.send(routerconfig["centeruser"] + "\r")
+        child.expect('Password: ', timeout=timeout)
+        sleep(sleepspan)
+        child.send(routerconfig["centerpassword"] + "\r")
 
-		# ユーザーログイン
-		child.expect('# ', timeout=timeout)
-		sleep(sleepspan)
-		child.send("telnet " + routerconfig["router"] + "\r")
-		child.expect('login: ', timeout=timeout)
-		sleep(sleepspan)
-		child.send(routerconfig["username"] + "\r")
-		child.expect('Password: ', timeout=timeout)
-		sleep(sleepspan)
-		child.send(routerconfig["password"] + "\r")
-		child.expect('# ', timeout=timeout)
+        # ユーザーログイン
+        child.expect('# ', timeout=timeout)
+        sleep(sleepspan)
+        child.send("telnet " + routerconfig["router"] + "\r")
+        child.expect('login: ', timeout=timeout)
+        sleep(sleepspan)
+        child.send(routerconfig["username"] + "\r")
+        child.expect('Password: ', timeout=timeout)
+        sleep(sleepspan)
+        child.send(routerconfig["password"] + "\r")
+        child.expect('# ', timeout=timeout)
 
-		# プロンプト文字列の取得
-		sleep(sleepspan)
-		child.send("! non-existent-lines\r") # ルータのヘッダー文字列に含まれない文字列をコメント送信することで、ヘッダーをスキップする
-		child.expect('non-existent-lines', timeout=timeout) # この時点では改行コードを含まない
-		prom = ""
-		prom1 = ""
-		while (prom1 != "#"):
-			prom = prom + prom1
-			c = child.read(1)
-			prom1 = chr(c[0])
-		
-		# 管理者モードに変更
-		sleep(sleepspan)
-		child.send('configure' + "\r")
-		child.expect(prom, timeout=timeout)
-		sleep(sleepspan)
-		child.send('svintr-config' + "\r")
-		child.expect(prom, timeout=timeout)
+        # プロンプト文字列の取得
+        sleep(sleepspan)
+        # ルータのヘッダー文字列に含まれない文字列をコメント送信することで、ヘッダーをスキップする
+        child.send("! non-existent-lines\r")
+        child.expect('non-existent-lines', timeout=timeout)  # この時点では改行コードを含まない
+        prom = ""
+        prom1 = ""
+        while (prom1 != "#"):
+            prom = prom + prom1
+            c = child.read(1)
+            prom1 = chr(c[0])
 
-		# config を送信する
-		for line in sendlines:
-			if (re.match(r"^\s*$", line)):
-				continue
-			sleep(sleepspan)
-			child.send(line + "\r")
-			child.expect(prom) # 標準のタイムアウト 30秒を利用する
+        # 管理者モードに変更
+        sleep(sleepspan)
+        child.send('configure' + "\r")
+        child.expect(prom, timeout=timeout)
+        sleep(sleepspan)
+        child.send('svintr-config' + "\r")
+        child.expect(prom, timeout=timeout)
 
-		sleep(sleepspan)
-		child.send('exit' + "\r")
-		child.expect(prom, timeout=timeout)
+        # config を送信する
+        for line in sendlines:
+            if (re.match(r"^\s*$", line)):
+                continue
+            sleep(sleepspan)
+            child.send(line + "\r")
+            child.expect(prom)  # 標準のタイムアウト 30秒を利用する
 
-		sleep(sleepspan)
-		child.send('exit' + "\r")
-		sleep(sleepspan)
-		child.send('exit' + "\r")
-		sleep(sleepspan)
-	finally:
-		logfp.flush()
-		logfp.close()
-		child.close()
+        sleep(sleepspan)
+        child.send('exit' + "\r")
+        child.expect(prom, timeout=timeout)
+
+        sleep(sleepspan)
+        child.send('exit' + "\r")
+        sleep(sleepspan)
+        child.send('exit' + "\r")
+        sleep(sleepspan)
+    finally:
+        logfp.flush()
+        logfp.close()
+        child.close()
+
+
+def ix_direct_login(routerconfig, sendlines, logfilename):
+    '''
+    ix_direct_login
+        IX にコンフィグ送信し、送信状態をログファイルに記録する関数 (ルーターを中継しない)
+            ファイルの中身は以下のような内容であるこのを期待している。1行目はJSON、2行目以降は ルータに送信したい内容
+            "router": "10.0.0.1", "routertype": "ix_direct_login", "username": "ログインユーザー名", "password": "ログインパスワード", "adminpassword": "管理者パスワード" }
+            show config
+    '''
+    sleepspan = 0.5
+    timeout = 5
+    # logfp = open(logfilename, 'wb')
+    logfp = multifile([sys.stdout.buffer, open(logfilename, 'wb')])
+    child = pexpect.spawn('telnet ' + routerconfig["router"])
+    child.logfile_read = logfp
+    try:
+        # ユーザーログイン
+        child.expect('login: ', timeout=timeout)
+        sleep(sleepspan)
+        child.send(routerconfig["username"] + "\r")
+        child.expect('Password: ', timeout=timeout)
+        sleep(sleepspan)
+        child.send(routerconfig["password"] + "\r")
+        child.expect('# ', timeout=timeout)
+
+        # プロンプト文字列の取得
+        sleep(sleepspan)
+        # ルータのヘッダー文字列に含まれない文字列をコメント送信することで、ヘッダーをスキップする
+        child.send("! non-existent-lines\r")
+        child.expect('non-existent-lines', timeout=timeout)  # この時点では改行コードを含まない
+        prom = ""
+        prom1 = ""
+        while (prom1 != "#"):
+            prom = prom + prom1
+            c = child.read(1)
+            prom1 = chr(c[0])
+
+        # 管理者モードに変更
+        sleep(sleepspan)
+        child.send('configure' + "\r")
+        child.expect(prom, timeout=timeout)
+        sleep(sleepspan)
+        child.send('svintr-config' + "\r")
+        child.expect(prom, timeout=timeout)
+
+        # config を送信する
+        for line in sendlines:
+            if (re.match(r"^\s*$", line)):
+                continue
+            sleep(sleepspan)
+            child.send(line + "\r")
+            child.expect(prom)  # 標準のタイムアウト 30秒を利用する
+
+        sleep(sleepspan)
+        child.send('exit' + "\r")
+        child.expect(prom, timeout=timeout)
+
+        sleep(sleepspan)
+        child.send('exit' + "\r")
+        sleep(sleepspan)
+        child.send('exit' + "\r")
+        sleep(sleepspan)
+    finally:
+        logfp.flush()
+        logfp.close()
+        child.close()
 
 
 def rtx_ssh_login(routerconfig, sendlines, logfilename):
-	'''
-	rtx_ssh_login
-	    RTX810, RTX1200 にコンフィグ送信し、送信状態をログファイルに記録する関数
-		ファイルの中身は以下のような内容であるこのを期待している。1行目はJSON、2行目以降は ルータに送信したい内容
-		"router": "10.0.0.1", "routertype": "rtx-ssh", "username": "ログインユーザー名", "password": "ログインパスワード", "adminpassword": "管理者パスワード" }
-		show config
-	'''
-	sleepspan = 0.5
-	timeout = 5
+    '''
+    rtx_ssh_login
+        RTX810, RTX1200 にコンフィグ送信し、送信状態をログファイルに記録する関数
+            ファイルの中身は以下のような内容であるこのを期待している。1行目はJSON、2行目以降は ルータに送信したい内容
+            "router": "10.0.0.1", "routertype": "rtx-ssh", "username": "ログインユーザー名", "password": "ログインパスワード", "adminpassword": "管理者パスワード" }
+            show config
+    '''
+    sleepspan = 0.5
+    timeout = 5
 
+    # logfp = open(logfilename, 'wb')
+    logfp = multifile([sys.stdout.buffer, open(logfilename, 'wb')])
+    child = pexpect.spawn('ssh -l ' + routerconfig["username"] + " " + routerconfig["router"])
+    child.logfile_read = logfp
+    try:
+        # ユーザーログイン
+        child.expect('password: ', timeout=timeout)
+        sleep(sleepspan)
+        child.send(routerconfig["password"] + "\n")
+        child.expect('> ', timeout=timeout)
 
-	# logfp = open(logfilename, 'wb')
-	logfp = multifile([ sys.stdout.buffer, open(logfilename, 'wb') ])
-	child = pexpect.spawn('ssh -l ' +  routerconfig["username"] + " " + routerconfig["router"])
-	child.logfile_read = logfp
-	try:
-		# ユーザーログイン
-		child.expect('password: ', timeout=timeout)
-		sleep(sleepspan)
-		child.send(routerconfig["password"] + "\n")
-		child.expect('> ', timeout=timeout)
+        # プロンプト文字列の取得
+        sleep(sleepspan)
+        # ルータのヘッダー文字列に含まれない文字列をコメント送信することで、ヘッダーをスキップする
+        child.send("# non-existent-lines\n")
+        child.expect('non-existent-lines', timeout=timeout)  # この時点では改行コードを含まない
+        prom = ""
+        prom1 = ""
+        while (prom1 != ">"):
+            prom = prom + prom1
+            c = child.read(1)
+            prom1 = chr(c[0])
 
-		# プロンプト文字列の取得
-		sleep(sleepspan)
-		child.send("# non-existent-lines\n") # ルータのヘッダー文字列に含まれない文字列をコメント送信することで、ヘッダーをスキップする
-		child.expect('non-existent-lines', timeout=timeout) # この時点では改行コードを含まない
-		prom = ""
-		prom1 = ""
-		while (prom1 != ">"):
-			prom = prom + prom1
-			c = child.read(1)
-			prom1 = chr(c[0])
-		
-		# 管理者モードに変更
-		sleep(sleepspan)
-		child.send('administrator' + "\n")
-		child.expect('Password: ', timeout=timeout)
+        # 管理者モードに変更
+        sleep(sleepspan)
+        child.send('administrator' + "\n")
+        child.expect('Password: ', timeout=timeout)
 
-		sleep(sleepspan)
-		child.send(routerconfig["adminpassword"] + "\n")
-		child.expect(prom, timeout=timeout)
+        sleep(sleepspan)
+        child.send(routerconfig["adminpassword"] + "\n")
+        child.expect(prom, timeout=timeout)
 
-		# config を送信する
-		for line in sendlines:
-			if (re.match(r"^\s*$", line)):
-				continue
-			sleep(sleepspan)
-			child.send(line + "\n")
-			child.expect(prom) # 標準のタイムアウト 30秒を利用する
+        # config を送信する
+        for line in sendlines:
+            if (re.match(r"^\s*$", line)):
+                continue
+            sleep(sleepspan)
+            child.send(line + "\n")
+            child.expect(prom)  # 標準のタイムアウト 30秒を利用する
 
-		sleep(sleepspan)
-		child.send('exit' + "\n")
-		child.expect(prom + '> ', timeout=timeout)
+        sleep(sleepspan)
+        child.send('exit' + "\n")
+        child.expect(prom + '> ', timeout=timeout)
 
-		sleep(sleepspan)
-		child.send('exit' + "\n")
-		sleep(sleepspan)
-	finally:
-		logfp.flush()
-		logfp.close()
-		child.close()
+        sleep(sleepspan)
+        child.send('exit' + "\n")
+        sleep(sleepspan)
+    finally:
+        logfp.flush()
+        logfp.close()
+        child.close()
